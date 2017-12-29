4c41edff0fe92ec22b639a913d051ba3cb73574e
tianshl <tianshl@qdum.com>
2017-12-29 14:40:28 +0800

    修改README

--- docs/_build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{"\u641c\u7d22\u9875\u9762":0,"\u6a21\u5757\u7d22\u5f15":0,"\u7d22\u5f15":0},titles:["wxReply"],titleterms:{"\u4ecb\u7ecd\u6587\u6863":[],"\u6b22\u8fce\u9605\u8bfb":[],document:[],indic:[],tabl:[],welcom:[],wxrepli:0}})
